c596187e4bd26cf70f6199f7ac3bdbd8f8d65d0c
Yakushin Alexandr <valensiec08@mail.ru>
2021-11-21 16:27:08 +0300

    Added manager

---
+++ src/main/java/ru/netology/manager/PostManeger.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManeger {
+    private Post[] posts;
+
+    public Post[] search(int OnwerId, String domain, String query, boolean OwnersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
